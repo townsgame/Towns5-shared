16dd9d885124556804596f30dfccb9098d6b8d06
phejny <ph@towns,cz>
2016-03-03 14:24:13 +0100

    [PH] Stories and terrains

---
+++ objects/stories/main.stories.js
@@ -0,0 +1,21 @@
+/**
+ * @author ©Towns.cz
+ * @author Pavol Hejný
+ * @fileOverview Building of main pack for migrations
+ */
+//======================================================================================================================
+
+//todo name of terrains
+module.exports=[
+    {
+        "name": "Příběh",
+        "type": "story",
+        "locale": "cs",
+        "content": {
+            "type": "markdown",
+            "data": "Kde bolo tam bolo"
+        }
+    }
+]
+;
+
---
+++ objects/terrains/main.terrains.js
@@ -0,0 +1,153 @@
+/**
+ * @author ©Towns.cz
+ * @author Pavol Hejný
+ * @fileOverview Building of main pack for migrations
+ */
+//======================================================================================================================
+
+//todo name of terrains
+module.exports=[
+    {
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:1,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:2,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:3,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:4,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:5,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:6,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:7,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:8,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:9,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:10,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:11,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:12,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:13,
+                size: 1
+            }
+        }
+    },{
+        "type": "terrain",
+        "name": "Terén",
+        "design": {
+            "type": "terrain",
+            "data":{
+                image:14,
+                size: 1
+            }
+        }
+    }
+]
+;
+
