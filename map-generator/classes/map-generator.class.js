46d4d8422db8635f9424c6b7d661c9a69b5a1fb5
Pavol Hejný <ph@towns.cz>
2016-04-19 22:34:11 +0200

    [PH] Interface refactoring

--- map-generator/classes/map-generator.class.js
+++ map-generator/classes/map-generator.class.js
@@ -130,8 +130,8 @@ Towns.MapGenerator.MapGenerator.prototype.convertMapArrayToObjects = function(ma
 
 
 
-
-Towns.MapGenerator.MapGenerator.prototype.getMap = function(center,radius){
+//private
+Towns.MapGenerator.MapGenerator.prototype.getPureMap = function(center,radius){
 
     center_integer={
         x: Math.floor(center.x),
@@ -147,42 +147,8 @@ Towns.MapGenerator.MapGenerator.prototype.getMap = function(center,radius){
 
 
 
-//======================================================================================================================
-
-
-
-
-
-Towns.MapGenerator.MapGenerator.prototype.getMapWithChanges = function(center,radius,material_terrains){
-    return objects;
-};
-
-
-
-
-Towns.MapGenerator.MapGenerator.prototype.getTerrainOnPosition = function(material_terrains,position){
-    return Terrain;
-};
-
-
-
-
-Towns.MapGenerator.MapGenerator.prototype.findNearestTerrain = function(material_terrains,position,terrain){
-    return 8;
-};
-
-
-
-
-
-
-//======================================================================================================================
-
-
-
-
-
-Towns.MapGenerator.MapGenerator.prototype.getVirtualObjects = function(objects){
+//private
+Towns.MapGenerator.MapGenerator.prototype.getVirtualObjectsFromTerrainObjects = function(objects){
 
     var self = this;
 
@@ -196,3 +162,150 @@ Towns.MapGenerator.MapGenerator.prototype.getVirtualObjects = function(objects){
     return(virtual_objects);
 
 };
+
+
+
+
+
+//======================================================================================================================
+//======================================================================================================================
+
+
+
+
+
+Towns.MapGenerator.MapGenerator.prototype.completeMap = function(real_objects,center,radius,virtual_objects){
+
+    var virtual_objects = virtual_objects || true;
+
+    var terrains_objects = this.getPureMap(center, radius);
+
+    terrains_objects.forEach(function(object){
+        real_objects.push(object);
+    });
+
+
+    if(virtual_objects){
+
+        console.log('Getting virtual objects');
+
+        var virtual_objects = this.getVirtualObjectsFromTerrainObjects(real_objects);
+
+        virtual_objects.forEach(function(object){
+            real_objects.push(object);
+        });
+
+    }else{
+
+        console.log('NOT Getting virtual objects');
+
+    }
+
+
+
+
+    return(real_objects);
+
+};
+
+
+
+
+
+
+Towns.MapGenerator.MapGenerator.prototype.getMapArray = function(real_objects,center,radius){
+
+
+    this.completeMap(real_objects,center,radius,false);
+
+    //--------------------------Create empty array
+    var map_array=[];
+    for (var y = 0; y < (radius*2); y++) {
+        map_array[y]=[];
+        for (var x = 0; x < (radius * 2); x++) {
+            map_array[y][x]=false;
+        }
+    }
+
+    //--------------------------
+
+    //--------------------------Fill array
+
+
+    real_objects.forEach(function(object){
+
+        if(object.type!='terrain')return;
+
+        if(object.design.data.size==1) {//todo is this optimalization effective?
+            //--------------------------
+
+            var x = Math.floor(object.x - center.x + radius);
+            var y = Math.floor(object.y - center.y + radius);
+
+            map_array[y][x]
+                =
+                T.MapGenerator.terrains[object.design.data.image];//todo maybe better
+
+            //--------------------------
+        }else {
+            //--------------------------
+
+            var x_from = Math.floor(object.x - center.x + radius - object.design.data.size);
+            var x_to = Math.ceil(object.x - center.x + radius + object.design.data.size);
+
+            var y_from = Math.floor(object.y - center.y + radius - object.design.data.size);
+            var y_to = Math.ceil(object.y - center.y + radius + object.design.data.size);
+
+
+            var xc = radius + center.x - object.x;
+            var yc = radius + center.y - object.y;
+
+
+            for (var y = y_from; y <= y_to; y++) {
+
+                if (typeof map_array[y] === 'undefined')continue;
+
+                for (var x = x_from; x <= x_to; x++) {
+
+
+                    if (typeof map_array[y][x] === 'undefined')continue;
+
+
+                    if (T.Math.xy2dist(x - xc, y - yc) <= object.design.data.size) {
+
+                        map_array[y][x]
+                            =
+                            T.MapGenerator.terrains[object.design.data.image];//todo maybe better
+
+                    }
+                }
+            }
+
+            //--------------------------
+        }
+
+    });
+    //--------------------------
+
+    return map_array;
+
+
+};
+
+
+
+/*
+Towns.MapGenerator.MapGenerator.prototype.getTerrainObjectOnPosition = function(real_objects,position){
+    return Terrain;
+};
+
+
+Towns.MapGenerator.MapGenerator.prototype.findNearestTerrain = function(material_terrains,position,terrain){
+    return 8;
+};*/
+
+
+
+
+
+
--- map-generator/samples/map-generator.js
+++ map-generator/samples/map-generator.js
@@ -8,17 +8,43 @@
 //Loading Instance of mapGenerator
 var mapGenerator = require('../inits/map-generator.init.js');
 
-    map_radius = 2,
+map_radius = 20,
     map_center = {
         x: 10000,
         y: 100
     };
+var ascii=['  ',' ~','##','~~','::','XX','Y@','~/','tt','--','||','^^','.~','\\\\','//'];
 
-var objects = mapGenerator.getMap(map_center,map_radius);
+
+var map = mapGenerator.getMapArray([],map_center,map_radius);
 
 
-console.log('List of virtual objects of type terrain near ['+map_center.x+','+map_center.y+']:');
-console.log(objects);
+for (var y = 0; y < map_radius * 2; y++) {
+
+    var row='';
+
+    for (var x = 0; x < map_radius * 2; x++) {
+
+        if (typeof(map[y][x]) === 'undefined'){
+
+            row+=ascii[0];
+
+        }else{
+
+            row+=ascii[map[y][x].design.data.image];
+
+        }
+
+
+
+
+    }
+
+    console.log(row);
+
+
+}
+
 
 
 
