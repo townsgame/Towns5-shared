696d791c441d87c4b49e710f1352d164b68815c9
phejny <ph@towns,cz>
2016-07-05 10:04:02 +0200

    [PH] User class converted to TS

--- ts/10-user.class.ts
+++ ts/10-user.class.ts
@@ -5,62 +5,77 @@
 //======================================================================================================================
 
 
-T.User = class{
 
+module T {
 
-    /**
-     * @param {object} user raw user data
-     */
-    constructor(user){
-
-        for(var key in user){
-            var this_key = key;
-            this[this_key] = user[key];
-        }
-
+    interface UserProfile {
+        username: 'string';
+        name: 'string';
+        surname: 'string';
+        email: 'string';
     }
 
 
-    /**
-     *
-     * @returns {string} HTML code of users signature
-     */
-    getSignatureHTML(){
+    export class User {
 
-        var name;
 
-        if(this.profile.name || this.profile.surname){
+        profile: UserProfile;
 
-            name = this.profile.name+' '+this.profile.surname;
+        /**
+         * @param {object} user raw user data
+         */
+        constructor(user: User) {
 
-        }else{
-
-            name = this.profile.username;
+            for (var key in user) {
+                var this_key = key;
+                this[this_key] = user[key];
+            }
 
         }
 
 
-        var email_md5 = md5(this.profile.email);
+        /**
+         *
+         * @returns {string} HTML code of users signature
+         */
+        getSignatureHTML():string {
 
-        
+            var name;
 
-        var signature_html = `
+            if (this.profile.name || this.profile.surname) {
+
+                name = this.profile.name + ' ' + this.profile.surname;
+
+            } else {
+
+                name = this.profile.username;
+
+            }
+
+
+            var email_md5 = md5(this.profile.email);
+
+
+            var signature_html = `
 
                 <div class="user-signature">
                     <img class="user-image" src="https://1.gravatar.com/avatar/` + email_md5 + `?s=80&r=pg&d=mm">
 
                     <div class="user-signature-text">
-                        <h1 class="user-name">`+name+`</h1>
-                        <p>`+this.profile.signature.html2text()+`</p>
+                        <h1 class="user-name">` + name + `</h1>
+                        <p>` + this.profile.signature.html2text() + `</p>
                     </div>
 
                 </div>
 
             `;
 
-        return(signature_html);
+            return (signature_html);
+
+        }
+
 
     }
 
 
-};
+}
