e4e5bf9439d529bd40efb8267e74a1879c48dc0a
pavolhejny <ph@towns.cz>
2016-07-26 00:36:40 +0200

    [typescript][PH] patches

--- ts/10-_position/20-area.class.ts
+++ ts/10-_position/20-area.class.ts
@@ -47,7 +47,7 @@ module T {
                     ib = i + 1;
                     if (ib == this.positions.length)ib = 0;
 
-                    collision = TMath.lineCollision(
+                    collision = T.TMath.lineCollision(
                         this.positions[ia].x,
                         this.positions[ia].y,
                         this.positions[ib].x,
